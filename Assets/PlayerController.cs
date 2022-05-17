c6a4c5c2a46a37fc33a33aae984e9ff7e897232d
FranSifon <fransifon@icloud.com>
2022-05-17 10:43:41 -0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 playerSpeed;
+    public float MovementSpeed;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(MovementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(MovementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, MovementSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, MovementSpeed, 0);
+        }
+
+    }
+}
---
+++ Assets/Vector3Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Script : MonoBehaviour
+{
+    public Vector3 miVector;
+    public GameObject myObject;
+
+    void Start()
+    {
+        myObject.transform.position = miVector;
+    }
+
+    void Update()
+    {
+        
+    }
+}
